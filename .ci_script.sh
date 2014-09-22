40a9e203dd6a918ee30cf55ba4101503893247e0
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2014-09-22 17:27:38 +0100

    Removed the reference to pushd and popd

--- .ci_script.sh
+++ .ci_script.sh
@@ -2,30 +2,23 @@
 set -x
 set -e
 
-baseFolder=`dirname $0`
-
-
 # Start frontEnd app
-pushd .
-cd "${baseFolder}/frontEnd"
+cd "./frontEnd"
 npm install
 npm run start
-popd
+cd ..
 
 # Wait until app starts
 sleep 1
 
 # Test the app
-pushd .
-cd "${baseFolder}/test"
+cd "./test"
 npm install
 npm test
-popd
-
+cd ..
 
 # Stop frontEnd app
-pushd .
-cd "${baseFolder}/frontEnd"
+cd "./frontEnd"
 npm install
 npm run stop
-popd
+cd ..
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 real-angular-beer
 =================
 
+[![Build Status](https://travis-ci.org/julianghionoiu/real-angular-beer.svg?branch=master)](https://travis-ci.org/julianghionoiu/real-angular-beer)
+
 A sample project that creates an Angular app with a Node.js backend
