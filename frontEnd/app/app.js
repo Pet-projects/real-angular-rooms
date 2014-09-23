9711ba62ab0514b923d4d1642f757176f9057b23
Rafael Audy Glanzner <rafael.audy@gmail.com>
2014-09-23 12:40:41 +0100

    Changing the name of the front end folder "src" to "app"

--- frontEnd/app/app.js
+++ frontEnd/app/app.js
@@ -6,17 +6,17 @@ angular.module('lateRooms', [
 config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
     $routeProvider.when('/rooms',     	{ 
-		templateUrl: '/src/domain/room/rooms.html', 
+		templateUrl: '/app/domain/room/rooms.html', 
     	controller: 'RoomsController' 
     });
 
     $routeProvider.when('/rooms/new',     	{ 
-		templateUrl: '/src/domain/room/room.html', 
+		templateUrl: '/app/domain/room/room.html', 
     	controller: 'RoomController' 
     });
 
     $routeProvider.when('/rooms/edit/:id',      { 
-        templateUrl: '/src/domain/room/room.html', 
+        templateUrl: '/app/domain/room/room.html', 
         controller: 'RoomController' 
     });
 
